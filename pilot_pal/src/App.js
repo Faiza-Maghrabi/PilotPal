8ea38f4ebfe56776b5f7fdaeaa15040281ee7917
aarthymahen <70587119+aarthymahen@users.noreply.github.com>
2023-03-13 18:05:30 +0000

    Updated the pull up menu and README.md including installation for dependency: react bootstrap

--- pilot_pal/src/App.js
+++ pilot_pal/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import MyMap from "./components/MyMap";
+import React from 'react';
+import MyMap from './components/MyMap';
+import PullUpMenu from './components/PullUpMenu';
 
 function App() {
-  return <MyMap />;
+  return (
+    <div>
+      <MyMap />
+      <PullUpMenu />
+    </div>
+  );
 }
 
 export default App;
---
+++ pilot_pal/src/components/PullUpMenu.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Button, Dropdown } from 'react-bootstrap';
+import './PullUpMenu.css';
+
+function PullUpMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Dropdown show={isOpen} autoClose="outside" onToggle={toggle}>
+      <Dropdown.Toggle as={Button} variant="Secondary" id="Dropdown">
+        Menu
+      </Dropdown.Toggle>
+      <Dropdown.Menu style={{ color: 'light blue' }}>
+        <Dropdown.Item>Option 1</Dropdown.Item>
+        <Dropdown.Item>Option 2</Dropdown.Item>
+        <Dropdown.Item>Option 3</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default PullUpMenu;
